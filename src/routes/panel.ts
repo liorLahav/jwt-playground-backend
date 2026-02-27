8962efcad646ffead671a6b3490face3980b6ebb
GalYazdi <galyazdi11@gmail.com>
2026-02-27 14:43:53 +0200

    small changes

--- src/routes/panel.ts
+++ src/routes/panel.ts
@@ -5,6 +5,7 @@ import { authorization } from "../middleware/authorization";
 export const panelRoutes = (server: FastifyInstance) => {
   server.get("/getTotalUsers", {preHandler: [authorization(["admin"])]}, getNumOfUsersHandler);
   server.get("/getAll", {preHandler: [authorization(["admin"])]}, getAllUsersHandler);
+  server.get("/getAllUnsecured", getAllUsersHandler);
   server.delete<{Params: {id: string}}>("/deleteUser/:id", {preHandler: [authorization(["admin"])]}, deleteUserHander);
   server.delete<{Params: {id: string}}>("/deleteUserUnSecured/:id", deleteUserHander);
 
